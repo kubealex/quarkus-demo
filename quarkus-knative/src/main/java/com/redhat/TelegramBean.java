e7586c098b5392a2b7b20ef54ea662b86f854ea5
kubealex <al.rossi87@gmail.com>
2022-03-24 16:27:31 +0100

    Telegram working

---
+++ quarkus-knative/src/main/java/com/redhat/TelegramBean.java
@@ -0,0 +1,34 @@
+package com.redhat;
+
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.apache.camel.component.telegram.model.IncomingMessage;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.quarkus.arc.Unremovable;
+
+@ApplicationScoped 
+@Unremovable
+public class TelegramBean {
+
+    @Inject 
+    KubernetesClient kubernetesClient;
+    
+    
+    public String translate(IncomingMessage message) throws UnknownHostException {    
+         // or Exchange, or org.apache.camel.component.telegram.model.IncomingMessage (or both)
+        switch (message.getText() ){
+            case "namespace":
+                return "Namespace is " + kubernetesClient.getNamespace();
+            case "hostname":
+                return "Hostname is " + InetAddress.getLocalHost().getHostName();            
+        }
+            return "First message!";
+        // do process 
+    }
+}
---
+++ quarkus-knative/src/main/java/com/redhat/TelegramRoute.java
@@ -0,0 +1,14 @@
+package com.redhat;
+import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.camel.builder.RouteBuilder;
+@ApplicationScoped
+public class TelegramRoute extends RouteBuilder {
+        @Override
+        public void configure() throws Exception {
+            from("telegram:bots?authorizationToken="+System.getenv("BOT_TOKEN"))
+                    .log("Incoming message ${body}")
+                    .bean(TelegramBean.class)
+                    .to("telegram:bots?authorizationToken="+System.getenv("BOT_TOKEN"));
+}
+}
