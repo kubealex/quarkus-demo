e73908522419c75c6b6d8d535c2b4ae70b10ce9f
kubealex <al.rossi87@gmail.com>
2022-04-27 17:11:01 +0200

    property on stress

--- quarkus-rest/src/main/java/org/acme/ApplicationService.java
+++ quarkus-rest/src/main/java/org/acme/ApplicationService.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 @Path("")
@@ -16,6 +17,15 @@ public class ApplicationService {
 
     @Inject
     ResponseService responseService;
+
+    @ConfigProperty(name = "cpu.cores", defaultValue = "8")
+    Integer numCore;
+    @ConfigProperty(name = "cpu.threads", defaultValue = "2")
+    Integer numThreadsPerCore;
+    @ConfigProperty(name = "cpu.load", defaultValue = "0.8")
+    Double load;
+    @ConfigProperty(name = "cpu.testDuration", defaultValue = "60000")
+    Long duration;
 
     @GET
     @Path("")
@@ -29,15 +39,12 @@ public class ApplicationService {
 
     @GET
     @Path("/stress")
-    public void stress() {
-        int numCore = 1;
-        int numThreadsPerCore = 2;
-        double load = 0.8;
-        final long duration = 50000;
+    public String stress() {
         for (int thread = 0; thread < numCore * numThreadsPerCore; thread++) {
             System.out.println("STRESSS");
             new BusyThread("Thread" + thread, load, duration).start();
         }
+        return "Stress test in place, look at autoscaling";
     }
 
 }
