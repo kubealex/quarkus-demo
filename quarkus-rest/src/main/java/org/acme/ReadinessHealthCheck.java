f1be892db9002aec5d06914628c7f02b5e0021ae
kubealex <al.rossi87@gmail.com>
2022-04-28 11:50:01 +0200

    added custom readyness

---
+++ quarkus-rest/src/main/java/org/acme/ReadinessHealthCheck.java
@@ -0,0 +1,21 @@
+package org.acme;
+
+import java.time.Duration;
+import javax.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+import io.smallrye.health.api.AsyncHealthCheck;
+import io.smallrye.mutiny.Uni;
+@Readiness
+@ApplicationScoped
+public class ReadinessHealthCheck implements AsyncHealthCheck {
+    @ConfigProperty(name = "readyness.delay", defaultValue = "0")
+    private Long readynessDelay;
+
+    @Override
+        public Uni<HealthCheckResponse> call() {
+            return Uni.createFrom().item(HealthCheckResponse.up("quarkus-rest-reactive"))
+                    .onItem().delayIt().by(Duration.ofSeconds(readynessDelay));
+        }    
+    }
